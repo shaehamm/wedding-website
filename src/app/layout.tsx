9c3ddcb4924ae7ad48120be78f0c3fc747e7aff2
ShaeHamm <shaehamm@gmail.com>
2024-03-20 14:49:53 -0500

    resolved import issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "..ui/globals.css";
+import "../ui/globals.css";
 import Header from "./components/header/header";
 import Navbar from "./components/nav/navbar";
 
