f6272b1230b77d8067a3c953c1e6580afd319fda
ShaeHamm <shaehamm@gmail.com>
2024-03-27 11:30:13 -0500

    added new header font

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,12 +1,13 @@
-import { WindSong } from "next/font/google";
+import { WindSong, Alegreya_Sans_SC } from "next/font/google";
 
 const windsong = WindSong({ weight: "400", subsets: ["latin"] });
+const alegreyaSansSC = Alegreya_Sans_SC({ weight: "300" });
 
 export default function Header() {
   return (
     <>
       <div className="text-center py-10">
-        <h1 className={windsong.className}>Shae & Connor</h1>
+        <h1 className={alegreyaSansSC.className}>Shae & Connor</h1>
         <h3>Saturday, February 15th, 2025 • Estes Park, Colorado</h3>
       </div>
     </>
