9a65d67ee1500e8e2cc882c4d16fd4021b431c2c
ShaeHamm <shaehamm@gmail.com>
2024-03-27 16:50:50 -0500

    added some text to wedding party page

--- src/app/wedding-party/page.tsx
+++ src/app/wedding-party/page.tsx
@@ -1,3 +1,16 @@
 export default function Page() {
-  return <p>Wedding Party Page</p>;
+  return (
+    <div className="columns-2 pt-5">
+      <div>
+        <h2>The Bridesmaids</h2>
+        <h3>Aidan</h3>
+        <p>Man of Honor</p>
+      </div>
+      <div>
+        <h2>The Groomsmen</h2>
+        <h3>James</h3>
+        <p>Best Man</p>
+      </div>
+    </div>
+  );
 }
