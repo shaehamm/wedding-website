9031f13ead4d55f0881d62f11bfbca28d9454055
ShaeHamm <shaehamm@gmail.com>
2024-03-28 14:12:21 -0500

    trying new formatting on wedding party

--- src/app/wedding-party/page.tsx
+++ src/app/wedding-party/page.tsx
@@ -4,22 +4,34 @@ const manropeLight = Manrope({ weight: "200", subsets: ["latin"] });
 
 export default function Page() {
   return (
-    <div className="columns-2 pt-5 text-left mx-10">
+    <div className="columns-2 pt-5 text-left px-2 0">
       <div>
-        <p className={`text-5xl ${manropeLight.className}`}>The Bridesmaids</p>
-        <p className="text 2xl">Aidan - Man of Honor</p>
-        <p>Karate</p>
+        <p className={`text-4xl ${manropeLight.className}`}>The Bridesmaids</p>
         <br />
-        <p className="text 2xl">Tipheret</p>
-        <p>High school</p>
-        <br />
-        <p className="text 2xl">Lauren</p>
-        <p>High school</p>
+        <div className="pl-10">
+          <p className="text-2xl">Aidan - Man of Honor</p>
+          <p>Karate</p>
+          <br />
+          <p className="text-2xl">Tipheret</p>
+          <p>High school</p>
+          <br />
+          <p className="text-2xl">Lauren</p>
+          <p>High school</p>
+        </div>
       </div>
       <div>
-        <h2 className={manropeLight.className}>The Groomsmen</h2>
-        <h3>James</h3>
-        <p>Best Man</p>
+        <p className={`text-4xl ${manropeLight.className}`}>The Groomsmen</p>
+        <br />
+        <div className="pl-10">
+          <p className="text-2xl">James - Best Man</p>
+          <p>College</p>
+          <br />
+          <p className="text-2xl">Will</p>
+          <p>College</p>
+          <br />
+          <p className="text-2xl">TBD</p>
+          <p>Unknown</p>
+        </div>
       </div>
     </div>
   );
