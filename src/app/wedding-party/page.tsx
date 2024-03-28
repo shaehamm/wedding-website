25c8255325dbdd34a20bb0bb74c0b46ba4b10791
ShaeHamm <shaehamm@gmail.com>
2024-03-28 14:03:15 -0500

    trying new formatting on wedding party

--- src/app/wedding-party/page.tsx
+++ src/app/wedding-party/page.tsx
@@ -4,11 +4,17 @@ const manropeLight = Manrope({ weight: "200", subsets: ["latin"] });
 
 export default function Page() {
   return (
-    <div className="columns-2 pt-5 text-left">
+    <div className="columns-2 pt-5 text-left mx-10">
       <div>
-        <h2 className={manropeLight.className}>The Bridesmaids</h2>
-        <h3>Aidan</h3>
-        <p>Man of Honor</p>
+        <p className={`text-5xl ${manropeLight.className}`}>The Bridesmaids</p>
+        <p className="text 2xl">Aidan - Man of Honor</p>
+        <p>Karate</p>
+        <br />
+        <p className="text 2xl">Tipheret</p>
+        <p>High school</p>
+        <br />
+        <p className="text 2xl">Lauren</p>
+        <p>High school</p>
       </div>
       <div>
         <h2 className={manropeLight.className}>The Groomsmen</h2>
