46a3f150e0d9197d3ab47a31f58c0b6f3fe15931
ShaeHamm <shaehamm@gmail.com>
2024-04-03 14:24:00 -0500

    working on footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -5,10 +5,20 @@ const alegreyaSansSC = Alegreya_Sans_SC({ weight: "300", subsets: ["latin"] });
 export default function Footer() {
   return (
     <>
-      <div className="text-center py-10">
-        <h2 className={alegreyaSansSC.className}>S&C</h2>
-        <h3>02.15.2025</h3>
-      </div>
+      <footer>
+        <div className="text-center py-10 col-span-3">
+          <div>
+            <p>/\/\/\</p>
+          </div>
+          <div>
+            <h2 className={alegreyaSansSC.className}>S&C</h2>
+            <h3>02.15.2025</h3>
+          </div>
+          <div>
+            <p>/\/\/\</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
